3cc117c620e4f8cfe246edc3cc2ffcb029e9201e
MauricioH <mauricioh12345678912@gmail.com>
2025-08-01 10:33:39 -0500

    backend: conexion del controlado con la ruta y el modelo con usuarios

--- backend/src/controllers/usuarios.js
+++ backend/src/controllers/usuarios.js
@@ -1,3 +1,9 @@
-class UsuarioController {
+import { Usuario } from "../models/usuario.js"
+
+export class UsuarioController {
     
+    static getAll = async (req, res)=>{
+        const result = await Usuario.getAll()
+        return res.status(201).json(result)
+    }
 }
--- backend/src/routes/usuarios.js
+++ backend/src/routes/usuarios.js
@@ -1,9 +1,6 @@
 import { Router } from "express";
-import { Usuario } from "../models/usuario.js";
+import {UsuarioController} from "../controllers/usuarios.js";
 
 export const usuariosRouter = Router()
 
-usuariosRouter.get('/', async (req, res)=>{
-    const result = await Usuario.getAll()
-    return res.status(201).json(result)
-})
+usuariosRouter.get('/', UsuarioController.getAll)
