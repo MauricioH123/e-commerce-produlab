fe8010400ee7b2e4b18fcac29a03893604967424
MauricioH <mauricioh12345678912@gmail.com>
2025-08-08 16:21:09 -0500

    backend: documentacion del metodo getAll del controaldor de categoria

--- backend/src/controllers/categorias.js
+++ backend/src/controllers/categorias.js
@@ -3,6 +3,50 @@ import { validatePartialCategory } from "../schemas/categoria.js";
 
 export class CategoryController {
 
+    /**
+     * @swagger
+     * /categorias:
+     *   get:
+     *     summary: Obtener todas las categorías
+     *     description: Retorna una lista con todas las categorías registradas.
+     *     tags: [Categories]
+     *     responses:
+     *       200:
+     *         description: Lista de categorías obtenida con éxito.
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: array
+     *               items:
+     *                 type: object
+     *                 properties:
+     *                   id:
+     *                     type: integer
+     *                     example: 1
+     *                   nombre:
+     *                     type: string
+     *                     example: Insumos médicos
+     *       404:
+     *         description: No existen categorías registradas.
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 error:
+     *                   type: string
+     *                   example: No existen categorias
+     *       500:
+     *         description: Error interno del servidor.
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 error:
+     *                   type: string
+     *                   example: No se pudo obtener las categorias
+     */
     static getAll = async (req, res) => {
         try {
             const categories = await Category.getAll()
