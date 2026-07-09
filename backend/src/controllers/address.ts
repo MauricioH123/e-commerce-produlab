4c11eb551c3a8507cb52ca20cdce23242c2f771a
MauricioH <mauricioh12345678912@gmail.com>
2026-07-09 17:20:50 -0500

    backend: eliminacion de las ruta de usuario

--- backend/src/controllers/address.ts
+++ backend/src/controllers/address.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from "express"
 export class Address {
 
     static getByUserId = async (req:Request, res: Response) => {
-
+        
     }
 
     static create = async (req: Request, res:Response , next:NextFunction ) => {
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -4,7 +4,3 @@ import {UsuarioController} from "../controllers/users.js";
 export const usersRouter = Router()
 
 usersRouter.get('/', UsuarioController.getAll)
-usersRouter.post('/', UsuarioController.createUser)
-usersRouter.delete('/:id', UsuarioController.delete)
-usersRouter.get('/:id/profile', UsuarioController.getProfile)
-usersRouter.put('/:id/profile', UsuarioController.update)
