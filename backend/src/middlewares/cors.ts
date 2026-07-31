578d3034075504df93a8316cc7e78858553d00a6
MauricioH <mauricioh12345678912@gmail.com>
2026-07-30 20:11:04 -0500

    backend: tipado de middleware de cors

--- backend/src/middlewares/cors.ts
+++ backend/src/middlewares/cors.ts
@@ -1,19 +1,20 @@
 import cors from 'cors'
 
-const ACCEPTED_ORIGINS = [
+const ACCEPTED_ORIGINS: string[] = [
     'http://localhost:8080',
     'http://localhost:3000'
 ]
 
-export const corsMiddleware = ({accepted_origins = ACCEPTED_ORIGINS} = {}) => cors({
-    origin: (origin, callback) => {
-        if (accepted_origins.includes(origin)) {
+interface CorsMiddlewareOptions {
+    accepted_origins?: string[]
+}
+
+export const corsMiddleware = ({accepted_origins = ACCEPTED_ORIGINS}: CorsMiddlewareOptions = {}) => cors({
+    origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+        if (!origin || accepted_origins.includes(origin)) {
             return callback(null, true)
         }
 
-        if (!origin) {
-            return callback(null, true)
-        }
         return callback(new Error('Not allowed by CORS'))
     }
 }) 
